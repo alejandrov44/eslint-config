370067c2be467f0f27efdd508d7d1af2b731eb44
alejandrov44 <31495891+alejandrov44@users.noreply.github.com>
2025-02-23 19:13:48 +0100

    add prettier configuration

---
+++ prettier.config.ts
@@ -0,0 +1,19 @@
+import { type Config } from "prettier";
+
+const config: Config = {
+    printWidth: 120,
+    tabWidth: 2,
+    useTabs: false,
+    semi: true,
+    quotes: false,
+    quoteProps: "as-needed",
+    jsxSingleQuote: false,
+    trailingComma: "all",
+    bracketSpacing: true,
+    objectWrap: "collapse",
+    bracketSameLine: true,
+    arrowParens: "always",
+    endOfLine: "lf",
+};
+
+export default config;
